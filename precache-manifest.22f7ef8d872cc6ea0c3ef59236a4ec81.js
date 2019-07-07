df1a83ff5b93d978d54e4f84e415ce6a768d0037
Arpit1993 <miliyearpitse@gmail.com>
2019-07-07 20:22:59 +0530

    Updates

---
+++ precache-manifest.22f7ef8d872cc6ea0c3ef59236a4ec81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db78828fcb24e7dfd645dedbdc9f753",
+    "url": "/react-basic/index.html"
+  },
+  {
+    "revision": "5b239b9a1f81b2151fec",
+    "url": "/react-basic/static/css/main.48759413.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-basic/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5b239b9a1f81b2151fec",
+    "url": "/react-basic/static/js/main.de346fa3.chunk.js"
+  },
+  {
+    "revision": "ab92bc48e7212b9f8fbe",
+    "url": "/react-basic/static/js/runtime~main.aa757889.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-basic/static/media/logo.5d5d9eef.svg"
+  }
+]);
